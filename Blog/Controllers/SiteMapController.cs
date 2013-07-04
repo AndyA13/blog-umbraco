141e0935dbc641dbea215899fc071f393236ee46
Andrew Aitken <hermanthesheep@gmail.com>
2013-07-04 17:38:10 +0100

    Started adding sitemap controller.

---
+++ Blog/Controllers/SiteMapController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class SiteMapController : Controller
+    {
+        //
+        // GET: /SiteMap/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
