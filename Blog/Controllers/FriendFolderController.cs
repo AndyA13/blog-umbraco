e06911e18d8396999545c93e4711bd3601be12bc
Andrew Aitken <hermanthesheep@gmail.com>
2013-07-04 15:54:17 +0100

    Added friend list view.

---
+++ Blog/Controllers/FriendFolderController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    using Blog.Models.DocumentTypes;
+    using Blog.Models.ViewModels;
+    using Umbraco.Web.Models;
+
+    using Vega.USiteBuilder;
+
+    public class FriendFolderController : RenderMvcController
+    {
+        /// <summary>
+        /// The friend list.
+        /// </summary>
+        /// <param name="renderModel">
+        /// The render model.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult FriendList(RenderModel renderModel)
+        {
+            FriendFolder model = ContentHelper.GetByNodeId<FriendFolder>(renderModel.Content.Id);
+
+            FriendListViewModel viewModel = new FriendListViewModel
+            {
+                Content = model
+            };
+
+            foreach (Friend friend in ContentHelper.GetChildren<Friend>(1091))
+            {
+                viewModel.Friends.Add(new FriendViewModel { Name = friend.Name, Url = friend.LinkUrl });
+            }
+
+            return this.View("FriendList", viewModel);
+        }
+    }
+}
---
+++ Blog/Models/ViewModels/FriendListViewModel.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FriendListViewModel.cs" company="Andrew Aitken">
+//   Andrew Aitken
+// </copyright>
+// <summary>
+//   The friend list view model.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Blog.Models.ViewModels
+{
+    using System.Collections.Generic;
+    using Blog.Models.DocumentTypes;
+
+    /// <summary>
+    /// The friend list view model.
+    /// </summary>
+    public class FriendListViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FriendListViewModel"/> class.
+        /// </summary>
+        public FriendListViewModel()
+        {
+            this.Friends = new List<FriendViewModel>();
+        }
+
+        /// <summary>
+        /// Gets or sets the content.
+        /// </summary>
+        public FriendFolder Content { get; set; }
+
+        /// <summary>
+        /// Gets or sets the friends.
+        /// </summary>
+        public List<FriendViewModel> Friends { get; set; }
+    }
+}
